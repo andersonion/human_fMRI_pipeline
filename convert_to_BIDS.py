8de59d52df0ee263944db0dbffc73a3017abd6b9
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-05-15 16:20:55 -0400

    Cleaning subject name per BIDS standards.

--- convert_to_BIDS.py
+++ convert_to_BIDS.py
@@ -85,7 +85,7 @@ if not os.path.exists(t1_json_path):
 
 func_path_orig = os.path.join(orig_path,f'{o_subj}_fMRI_nii4D.nii.gz')  # change this with your file
 func_json_path_orig = func_path_orig.replace(".nii.gz", ".json")
-
+print(func_json_path_orig)
 # BIDS standards -- Do not change
 func_nii_path = os.path.join(func_folder,f'sub-{subj}_task-rest_bold.nii.gz')
 func_json_path = func_nii_path.replace(".nii.gz", ".json")
