d87886fbdcd73cb2b304856cfe14a92729ba460f
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-05-27 11:46:17 -0400

    Added circular conda call to fmri_connectomes_v2.py.

--- ADRC_fmri_prep_pipeline/fmri_connectomes_v2.py
+++ ADRC_fmri_prep_pipeline/fmri_connectomes_v2.py
@@ -14,7 +14,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 conda_env = os.environ.get("CONDA_DEFAULT_ENV")
 
-if conda_env not "fmri_connectomes":
+if conda_env != "fmri_connectomes":
 	print(f"Conda environment 'fmri_connectomes' not activated; running setup/activate script now...")
 	subprocess.run([f"bash {script_dir}/setup_fmri_connectomes_conda_env.bash {script_path}"])
 
