455a5ef31f7803a9cef20a991b2e07a102e392a4
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-06-02 16:10:50 -0400

    Debugging fmriconnectome code.

--- ADRC_fmri_prep_pipeline/setup_fmri_connectomes_conda_env.bash
+++ ADRC_fmri_prep_pipeline/setup_fmri_connectomes_conda_env.bash
@@ -57,6 +57,7 @@ ENV_DECL+=" PATH=/usr/bin:/bin"
 eval env -i $ENV_DECL bash --noprofile --norc <<EOF
 echo "DEBUG TESTING:"
 echo $PATH
+conda info --envs | grep '/fmri_connectomes' 2>/dev/null | wc -l
 conda_test=$(conda info --envs | grep '/fmri_connectomes' 2>/dev/null | wc -l);
 if ((! ${conda_test} ));then
 	echo "Conda environment 'fmri_connectomes' does not appear to exist; configuring now..."
